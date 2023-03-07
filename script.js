3c7f63bee0dda423af3dfae0aba0a1ccb83460f2
Mahabub <mahabubx7@gmail.com>
2023-03-07 19:52:27 +0600

    :rocket: coded pop-up ui

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const mobileMenu = document.getElementById('mobile-menu');
 const menuItems = document.querySelectorAll('#mobile-menu a');
 const hamberger = document.getElementById('hamberger');
 const close = document.getElementById('close-menu');
+const modalOpen = document.querySelectorAll('.btn-project');
+const modalClose = document.querySelectorAll('.close-modal');
+const modal = document.querySelector('.modal');
 
 // re-usable code
 function closeMenu() {
@@ -11,8 +14,27 @@ function closeMenu() {
   }
 }
 
+function closeModal() {
+  if (!modal.classList.contains('d-none')) {
+    modal.classList.add('d-none');
+  }
+}
+
 // all-interaction-scripts
 function interactions() {
+  // modal
+  modalOpen.forEach((it) => {
+    it.addEventListener('click', (event) => {
+      modal.classList.remove('d-none');
+      event.preventDefault();
+    });
+  });
+
+  modalClose.forEach((it) => {
+    it.addEventListener('click', closeModal);
+  });
+
+  // mobile menu
   hamberger.addEventListener('click', () => {
     mobileMenu.classList.remove('d-none');
   });
