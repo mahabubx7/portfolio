f44b3476b6eb13450767f6fc6a2640bf9db69928
sebagabo <sebagabojeanaime1998@gmai.com>
2023-03-08 15:20:27 +0100

    :zap: added-form-validation

--- script.js
+++ script.js
@@ -27,3 +27,32 @@ function interactions() {
 }
 
 interactions(); // scripts setting up
+
+
+// validation for contact form
+const form = document.getElementById('contactForm');
+
+function checkEmail(str) {
+  return typeof(str) === 'string' && str === str.toLowerCase();
+}
+
+let formData = {};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  formData = Object.assign({
+    name: event.currentTarget.name.value || '',
+    email: event.currentTarget.email.value || '',
+    feedback: event.currentTarget.feedback.value || '',
+  });
+
+  if (checkEmail(formData['email'])) {
+    form.submit();
+  } else {
+    // console.log("Error!", formData);
+    const error = document.createElement('p');
+    error.innerText = "Invalid Email Address!";
+    error.classList.add('error');
+    form.appendChild(error);
+  }
+});
